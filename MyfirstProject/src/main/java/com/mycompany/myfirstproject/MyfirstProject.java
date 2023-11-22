f1d720c970f6197a299fd7cca9e6d53d916f1d8c
cathcath16 <catherenelabinisia@gmail.com>
2023-11-22 20:04:19 +0800

    first commit

---
+++ MyfirstProject/src/main/java/com/mycompany/myfirstproject/MyfirstProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.myfirstproject;
+
+/**
+ *
+ * @author Acer
+ */
+public class MyfirstProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
