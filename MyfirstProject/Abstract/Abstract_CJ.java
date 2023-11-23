548f8f6a0e48532f0bce403431d92803087e3eef
cathcath16 <catherenelabinisia@gmail.com>
2023-11-23 23:51:35 +0800

    adding my files

---
+++ MyfirstProject/Abstract/Abstract_CJ.java
@@ -0,0 +1,8 @@
+package abstractt;
+
+public class Abstract_CJ extends Abstract_Student{
+    
+    public void studentCute(){
+        System.out.println("Nagpa Gwapa si CJ");
+    }
+}
---
+++ MyfirstProject/Abstract/Abstract_Student.java
@@ -0,0 +1,12 @@
+package abstractt;
+
+abstract class Abstract_Student {
+    public abstract void studentCute();
+        
+    public void eat(){
+        System.out.println("Eating time naaaa");
+    }
+    public void sleep(){
+        System.out.println("Sleeeey na meeeeee");
+    }
+}
